d81888769a597b506f0ce31ea3e6613d919cbf13
Srihari1878 <99256131+Srihari1878@users.noreply.github.com>
2022-02-10 15:14:02 +0530

    Add files via upload

---
+++ js/forin/main.js
@@ -0,0 +1,11 @@
+//forof loop
+let objec = {
+    name: "hari",
+    place: "madurai"
+}
+
+for(let i of objec){
+    document.write(i)
+}
+// objec is a object type, so as mentioned in console, 
+// objectis not iterable
